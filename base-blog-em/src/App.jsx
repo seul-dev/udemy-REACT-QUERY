ec0c8cfb1f22d8665aa8f544cc6f770cf65ab6d3
seul-dev <seul.dev@gmail.com>
2023-03-01 17:19:03 +0900

    useQuery, caching

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,20 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { Posts } from './Posts';
+import './App.css';
+
+export const queryClient = new QueryClient();
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      <div className='App'>
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,41 +1,76 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
+import { queryClient } from './App';
+
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNumber) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const {
+    data: posts,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(['posts', currentPage], () => fetchPosts(currentPage), {
+    staleTime: 1000 * 60,
+  });
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPageNum = currentPage + 1;
+      queryClient.prefetchQuery(
+        ['posts', nextPageNum],
+        () => fetchPosts(nextPageNum),
+        {
+          staleTime: 1000 * 60,
+        }
+      );
+    }
+  }, [currentPage]);
+
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) return <h3>Something went wrong {error.toString()}</h3>;
 
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {posts.map((post) => (
           <li
             key={post.id}
-            className="post-title"
+            className='post-title'
             onClick={() => setSelectedPost(post)}
           >
             {post.title}
           </li>
         ))}
       </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
+      <div className='pages'>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
           Next page
         </button>
       </div>
