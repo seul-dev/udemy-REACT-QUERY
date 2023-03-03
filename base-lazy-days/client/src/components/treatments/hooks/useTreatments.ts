94f70396f080a6b243debb9aec9446d5e3f3d2e7
seul-dev <seul.dev@gmail.com>
2023-03-03 13:17:54 +0900

    centralize error handling for query

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,24 +3,14 @@ import { useQuery } from '@tanstack/react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
-// for when we need a query function for useQuery
 async function getTreatments() {
   const { data } = await axiosInstance.get<Treatment[]>('/treatments');
   return data;
 }
 
 export function useTreatments(): Treatment[] {
-  const toast = useCustomToast();
-
   const fallback: Treatment[] = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    onError: (error) => {
-      const title =
-        error instanceof Error ? error.message : 'error connectint to server';
-      toast({ title, status: 'error' });
-    },
-  });
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
