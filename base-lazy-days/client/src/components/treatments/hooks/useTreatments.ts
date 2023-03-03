33fe0505b9a7bdba900070f7076eb72b7eff377f
seul-dev <seul.dev@gmail.com>
2023-03-03 10:22:00 +0900

    custom useQuery hook, fallback data, useIsFetching

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments() {
+  const { data } = await axiosInstance.get<Treatment[]>('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const toast = useCustomToast();
+
+  const fallback: Treatment[] = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error ? error.message : 'error connectint to server';
+      toast({ title, status: 'error' });
+    },
+  });
+  return data;
 }
