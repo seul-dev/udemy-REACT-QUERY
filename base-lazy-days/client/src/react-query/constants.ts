8aee1625f5fb2c063bc68997883baf522f264765
seul-dev <seul.dev@gmail.com>
2023-03-05 19:03:53 +0900

    queryKey 파일 수정

---
+++ base-lazy-days/client/src/react-query/constants.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export const queryKeys = {
+  treatments: ['treatments'],
+  appointments: (year: string, month: string) => ['appointments', year, month],
+  user: ['user'],
+  staff: ['staff'],
+};
